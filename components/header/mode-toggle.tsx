330c249c2135a8a5228edac08b6ff7a1762e284d
Abhishek Mishra <mabhishek0221@gmail.com>
2024-04-05 17:55:12 -0400

    edit:theme-filter

--- components/header/mode-toggle.tsx
+++ components/header/mode-toggle.tsx
@@ -4,27 +4,16 @@ import * as React from "react";
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
 import { MoonIcon, SunIcon } from "lucide-react";
-import commonStrings from "@/lib/strings/common.json";
 
 export function ModeToggle() {
   const { setTheme } = useTheme();
 
   return (
     <>
-      <Button
-        variant="outline"
-        size="icon"
-        onClick={() => setTheme(commonStrings.theme.lightTheme)}
-        className="hidden dark:flex"
-      >
+      <Button variant="outline" size="icon" onClick={() => setTheme("light")} className="hidden dark:flex">
         <MoonIcon className="h-[1.2rem] w-[1.2rem] transition-all" />
       </Button>
-      <Button
-        variant="outline"
-        size="icon"
-        onClick={() => setTheme(commonStrings.theme.darkTheme)}
-        className="dark:hidden"
-      >
+      <Button variant="outline" size="icon" onClick={() => setTheme("dark")} className="dark:hidden">
         <SunIcon className="h-[1.2rem] w-[1.2rem] transition-all" />
       </Button>
     </>
