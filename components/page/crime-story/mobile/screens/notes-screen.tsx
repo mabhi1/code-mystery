af135184b216954e75d03eebbcadce7ba4ed516b
Abhishek Mishra <mabhishek0221@gmail.com>
2024-04-12 01:18:53 -0400

    edit:crime-story

--- components/page/crime-story/mobile/screens/notes-screen.tsx
+++ components/page/crime-story/mobile/screens/notes-screen.tsx
@@ -6,7 +6,7 @@ export default function NotesScreen({ setScreen }: { setScreen: Dispatch<SetStat
     <div className="flex flex-col gap-5">
       <ScreenHeader setScreen={setScreen}>Notes</ScreenHeader>
       <div className="text-base">My Address</div>
-      <div className="uppercase">111 first street, orlando, florida, 21345</div>
+      <div className="uppercase">111 first street, orlando, florida, 21245</div>
     </div>
   );
 }
